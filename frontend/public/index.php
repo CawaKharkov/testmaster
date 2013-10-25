fb7ba611403766bab9a4edc1242a9d24cf978d62
Cawa <cawa123@mail.ru>
2013-10-26 01:08:17 +0300

    base structure

---
+++ frontend/public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TestMaster.com</title>
+    </head>
+    <body>
+
+        <div ng-app="">
+            {{2 + 2}}
+        </div>
+
+        <script type="text/javascript" src="angularJs//angular.min.js"></script>
+    </body>
+</html>
